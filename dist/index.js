31fab72f8e8dce79aef680a101b85c177a6383a4
arunkjojo <arunkjojo@gmail.com>
2022-10-17 12:05:19 +0530

    ch2 - Use Express Body

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
+app.use(body_parser_1.default.urlencoded({
+    extended: false
+}));
+app.use(body_parser_1.default.json());
 app.get('/', (req, res) => {
-    res.send('Hello Arun');
+    res.send('Hello World');
+});
+app.post('/', (req, res) => {
+    res.send({
+        data: req.body
+    });
 });
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World');
+})
+
+app.post('/', (req, res) => {
+    res.send({
+        data: req.body
+    });
 })
 
 app.listen(3000, () => {
